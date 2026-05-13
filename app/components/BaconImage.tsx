1af299118583511d58daca812c4596dae65a29fe
Bilbis <bilbis-bot@bilbis.ai>
2026-05-13 22:04:34 +0000

    feat(KAI-60): Fronted: add random gif instead of bacon and change background to green

--- app/components/BaconImage.tsx
+++ app/components/BaconImage.tsx
@@ -1,17 +1,44 @@
 'use client';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+import { useEffect, useState } from 'react';
 
-interface BaconImageProps {
+interface RandomGifImageProps {
   width?: number;
   height?: number;
 }
 
-export default function BaconImage({ width = 300, height = 300 }: BaconImageProps) {
-  const src = `${BACKEND_URL}/api/bacon?width=${width}&height=${height}`;
+export default function BaconImage({
+  width = 300,
+  height = 300,
+}: RandomGifImageProps) {
+  const [gifSrc, setGifSrc] = useState<string>('');
+
+  useEffect(() => {
+    const gif =
+      // Random gif from public domain endpoint; avoids backend dependency.
+      `https://media.giphy.com/media/${randomGifId()}/giphy.gif`;
+
+    setGifSrc(gif);
+  }, []);
+
+  if (!gifSrc) {
+    return null;
+  }
 
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src={src} alt="Bacon" width={width} height={height} />
+    <img src={gifSrc} alt="Random GIF" width={width} height={height} />
   );
+}
+
+function randomGifId() {
+  const ids = [
+    '3o6Zt481isNVuQI1J6',
+    'l0MYt5jPR6QX5pnqM',
+    'xT9IgzoKnwFNmISRqI',
+    '26BRv0ThflsHCqDr2',
+    '3o7aD2saalBwwftBIY',
+  ];
+
+  return ids[Math.floor(Math.random() * ids.length)] ?? ids[0];
 }
